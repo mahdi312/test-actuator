07fb25916a5b4200c83a8dbae16de0dd96be9e10
mahdi312 <mahdi.mostafavi312@gmail.com>
2023-07-18 01:06:35 +0330

    start project with one endpoint /actuator/health

---
+++ actuator/src/test/java/com/mah312/actuator/ActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mah312.actuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
